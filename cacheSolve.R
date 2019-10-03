0d95be3cb58704789b28188e05aa975a63c01553
Sandeepsingh-k <52542272+Sandeepsingh-k@users.noreply.github.com>
2019-10-03 21:55:45 +0530

    Add files via upload

---
+++ cacheSolve.R
@@ -0,0 +1,16 @@
+
+## This function computes the inverse of the special "matrix" created by 
+## makeCacheMatrix above.
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
+}
